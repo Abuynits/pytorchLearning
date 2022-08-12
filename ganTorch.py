85425850f0be8de95eda13a9d6fe62b2bec8d8b5
Alexiy Buynitsky <abuynits@gmail.com>
2022-08-12 15:43:23 -0700

    added notes on gan networks;

---
+++ ganTorch.py
@@ -0,0 +1,7 @@
+"""
+lookning at generative adversarial networks in torch
+we use dnn for supervised learning: identify which image or which object in the image
+supervised: have some data and then create a model that do similar predictions on other nn
+generative modeling: unsupervised learning task: only the data
+involves learnign the data so that you can output new examples that you can ge tfrom original dataset that look similar to original ds
+"""
